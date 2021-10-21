85a9846077f6f9d6126d3fc95537c2093f0d7936
DmytroNakonechnyi <vayren0965@gmail.com>
2021-10-21 17:42:58 +0300

    let's start with react

--- stacks/index.js
+++ stacks/index.js
@@ -1,10 +1,5 @@
 import FrontendStack from './FrontendStack';
 
 export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs12.x"
-  });
-
   new FrontendStack(app, 'frontend');
 }
